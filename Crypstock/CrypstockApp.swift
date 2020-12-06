488da5517eae3e8c0d118fcacea5fe6a0d42c9a0
Maxence Marchal <maxence.marchal@gmail.com>
2020-12-06 19:38:19 +0100

    Did some prototype for UI with SwiftUI stacks

--- Crypstock/CrypstockApp.swift
+++ Crypstock/CrypstockApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct CrypstockApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            SignInView()
         }
     }
 }
---
+++ Crypstock/SignInView.swift
@@ -0,0 +1,34 @@
+//
+//  SignInView.swift
+//  Crypstock
+//
+//  Created by Maxence Marchal on 06/12/2020.
+//
+
+import SwiftUI
+
+struct SignInView: View {
+    let backgroundGradientColors: Gradient = Gradient(colors: [Color(red: 0.0, green: 0.7058823529, blue: 0.8588235294), Color(red: 0, green: 0.5137254902, blue: 0.6901960784)])
+    var body: some View {
+        ZStack {
+            LinearGradient(gradient: backgroundGradientColors, startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
+            VStack {
+            Image("logo")
+                Spacer()
+                ZStack {
+                    RoundedRectangle(cornerRadius: 20).foregroundColor(.white).frame(minWidth: 0, maxWidth: 350, maxHeight: 450, alignment: .center)
+                    HStack {
+                        Text("Sign In")
+                    }
+                }
+                Spacer()
+            }
+        }
+    }
+}
+
+struct SignInView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignInView()
+    }
+}
