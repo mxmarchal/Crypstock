f5dfb0c821d7022d9d076aee3799293e9076f278
Maxence Marchal <maxence.marchal@gmail.com>
2020-12-06 17:59:27 +0100

    Initial Commit

---
+++ Crypstock/CrypstockApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrypstockApp.swift
+//  Crypstock
+//
+//  Created by Maxence Marchal on 06/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct CrypstockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
