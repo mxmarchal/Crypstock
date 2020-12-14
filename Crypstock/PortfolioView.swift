94315d9ea9208e1ad152dfe06c676e9ec1f8be19
Maxence Marchal <maxence.marchal@gmail.com>
2020-12-14 06:47:23 +0100

    Improve my graph generator by tweaking some options + add a generator for dataset + did some little update on coins' UI

--- Crypstock/PortfolioView.swift
+++ Crypstock/PortfolioView.swift
@@ -10,25 +10,28 @@ import Combine
 
 struct CoinItemView: View {
     let name: String;
+    let shortName: String;
+    let currentValue: Double;
     let primaryColor: Color;
     let secondaryColor: Color;
     
     var body: some  View {
+        let currentValueFormatted = String(format: "%.2f", currentValue)
+        let randomDataGraph = generateRandomDataArray()
         HStack {
             Group {
                 VStack(alignment: .leading) {
-                    Text("\(name)")
-                        .font(.largeTitle)
-                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-                    /*
-                    Path { path in
-                        path.move(to: CGPoint(x: 0, y: 0))
-                        path.addLine(to: CGPoint(x: 50, y: 50))
-                        path.addLine(to: CGPoint(x: 350.56, y: 0))
+                    HStack {
+                        Text("\(name)")
+                            .font(.largeTitle)
+                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                        Spacer()
+                        VStack(alignment: .trailing) {
+                            Text("1\(shortName) = \(currentValueFormatted)$").fontWeight(.semibold)
+                            Text("12390 $USD").fontWeight(.bold).font(.title2)
+                        }
                     }
-                    .stroke(Color.white, lineWidth: 2)
-                    */
-                    CoinItemGraph(graphData: [0, 23, 34, 46, 16, 45, 90, 66])
+                    CoinItemGraph(graphData: randomDataGraph)
                 }
             }.padding(20)
         }
@@ -38,10 +41,23 @@ struct CoinItemView: View {
         .cornerRadius(20.0)
     }
     
+    func generateRandomDataArray() -> [Double] {
+        var data: [Double] = []
+        let minValue = currentValue - (currentValue * 0.2)
+        let maxValue = currentValue + (currentValue * 0.2)
+        
+        for _ in 0..<50 {
+            let value = Double.random(in: minValue ..< maxValue)
+            data.append(value)
+        }
+        print(data)
+        return data
+    }
+    
     struct CoinItemGraph: View {
         let graphData: [Double]
-        let maxWidth: Double = 200.0;
-        let maxHeight: Double = 35.0;
+        let maxWidth: Double = 300.0;
+        let maxHeight: Double = 200.0;
 
         func generateGraph() -> Path {
             var graph = Path()
@@ -51,7 +67,8 @@ struct CoinItemView: View {
             
             graph.move(to: CGPoint(x: 0, y: 0))
             for dataIndex in 0..<graphData.count {
-                let p2 = CGPoint(x: stepWidth * Double(dataIndex), y: graphData[dataIndex] - min / diff * maxHeight)
+                let p2 = CGPoint(x: stepWidth * Double(dataIndex), y: ((graphData[dataIndex] - min) / diff) * maxHeight)
+                print(p2)
                 graph.addLine(to: p2)
             }
             return graph;
@@ -68,14 +85,16 @@ struct CoinItemView: View {
 struct Coin: Identifiable {
     var id = UUID()
     var name: String
+    var shortName: String
+    var currentValue: Double
     var primaryColor: String
     var secondaryColor: String
 }
 
 struct PortfolioView: View {
     let coinsData: [Coin] =
-        [Coin(name: "Bitcoin", primaryColor: "#FDC830", secondaryColor: "#F37335"),
-         Coin(name: "Etherum", primaryColor: "#8E2DE2", secondaryColor: "#4A00E0")
+        [Coin(name: "Bitcoin", shortName: "BTC", currentValue: 15721.79, primaryColor: "#FDC830", secondaryColor: "#F37335"),
+         Coin(name: "Ethereum", shortName: "ETH", currentValue: 584.67, primaryColor: "#8E2DE2", secondaryColor: "#4A00E0")
         ]
 
     var body: some View {
@@ -83,7 +102,7 @@ struct PortfolioView: View {
             List(coinsData) {
                 coin in
                     ZStack {
-                        CoinItemView(name: coin.name, primaryColor: Color(hex: coin.primaryColor), secondaryColor: Color(hex: coin.secondaryColor))
+                        CoinItemView(name: coin.name, shortName: coin.shortName, currentValue: coin.currentValue, primaryColor: Color(hex: coin.primaryColor), secondaryColor: Color(hex: coin.secondaryColor))
                         NavigationLink(destination: /*@START_MENU_TOKEN@*/Text("Destination")/*@END_MENU_TOKEN@*/) {
                         }.opacity(0.0)
                     }
