6ef7a26fc9cf0054969f6edde3fd7daf8f022454
Maxence Marchal <maxence.marchal@gmail.com>
2020-12-14 06:19:36 +0100

    Add graph generation

--- Crypstock/PortfolioView.swift
+++ Crypstock/PortfolioView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Combine
 
 struct CoinItemView: View {
     let name: String;
@@ -15,9 +16,20 @@ struct CoinItemView: View {
     var body: some  View {
         HStack {
             Group {
-                Text("\(name)")
-                    .font(.largeTitle)
-                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                VStack(alignment: .leading) {
+                    Text("\(name)")
+                        .font(.largeTitle)
+                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    /*
+                    Path { path in
+                        path.move(to: CGPoint(x: 0, y: 0))
+                        path.addLine(to: CGPoint(x: 50, y: 50))
+                        path.addLine(to: CGPoint(x: 350.56, y: 0))
+                    }
+                    .stroke(Color.white, lineWidth: 2)
+                    */
+                    CoinItemGraph(graphData: [0, 23, 34, 46, 16, 45, 90, 66])
+                }
             }.padding(20)
         }
         .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, idealWidth: 300, maxWidth: .infinity, minHeight: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, idealHeight: 180, maxHeight: 350, alignment: .topLeading)
@@ -25,21 +37,54 @@ struct CoinItemView: View {
         .foregroundColor(.white)
         .cornerRadius(20.0)
     }
+    
+    struct CoinItemGraph: View {
+        let graphData: [Double]
+        let maxWidth: Double = 200.0;
+        let maxHeight: Double = 35.0;
+
+        func generateGraph() -> Path {
+            var graph = Path()
+            let stepWidth: Double = maxWidth / Double(graphData.count)
+            let min: Double = graphData.min() ?? 0
+            let diff: Double = graphData.max() ?? 0 - min
+            
+            graph.move(to: CGPoint(x: 0, y: 0))
+            for dataIndex in 0..<graphData.count {
+                let p2 = CGPoint(x: stepWidth * Double(dataIndex), y: graphData[dataIndex] - min / diff * maxHeight)
+                graph.addLine(to: p2)
+            }
+            return graph;
+        }
+        
+        var body: some View {
+            generateGraph().stroke(Color.white ,style: StrokeStyle(lineWidth: 2, lineJoin: .round))
+                .rotationEffect(.degrees(180), anchor: .center)
+                .rotation3DEffect(.degrees(180), axis: (x: 0, y: 1, z: 0))
+        }
+    }
+}
+
+struct Coin: Identifiable {
+    var id = UUID()
+    var name: String
+    var primaryColor: String
+    var secondaryColor: String
 }
 
 struct PortfolioView: View {
+    let coinsData: [Coin] =
+        [Coin(name: "Bitcoin", primaryColor: "#FDC830", secondaryColor: "#F37335"),
+         Coin(name: "Etherum", primaryColor: "#8E2DE2", secondaryColor: "#4A00E0")
+        ]
+
     var body: some View {
         NavigationView {
-            List {
-                ZStack {
-                    CoinItemView(name: "Bitcoin", primaryColor: Color(hex: "#FDC830"), secondaryColor: Color(hex: "#F37335"))
-                    NavigationLink(destination: /*@START_MENU_TOKEN@*/Text("Destination")/*@END_MENU_TOKEN@*/) {
-                    }.opacity(0.0)
-                }
-                
+            List(coinsData) {
+                coin in
                     ZStack {
-                        CoinItemView(name: "Bitcoin", primaryColor: Color(hex: "#FDC830"), secondaryColor: Color(hex: "#F37335"))
-                        NavigationLink(destination: Text("Destination 2")) {
+                        CoinItemView(name: coin.name, primaryColor: Color(hex: coin.primaryColor), secondaryColor: Color(hex: coin.secondaryColor))
+                        NavigationLink(destination: /*@START_MENU_TOKEN@*/Text("Destination")/*@END_MENU_TOKEN@*/) {
                         }.opacity(0.0)
                     }
             }
