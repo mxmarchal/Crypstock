1a199bd25be6f6ada219ad20ea336a2a85c38443
Maxence Marchal <maxence.marchal@gmail.com>
2021-01-08 00:36:28 +0100

    Add coins available pages + some details about coins and possibility to buy / sell

---
+++ Crypstock/Views/ListCoinsView.swift
@@ -0,0 +1,45 @@
+//
+//  ListCoinsView.swift
+//  Crypstock
+//
+//  Created by Maxence Marchal on 07/01/2021.
+//
+
+import SwiftUI
+
+struct ListCoinsView: View {
+    @Binding var childView: String
+    @Binding var user: User
+    
+    let coinsJSON: [Coin]? = Coins().loadCoinsFromJSON()
+
+    var body: some View {
+        NavigationView {
+            if let coinsData = coinsJSON {
+                List(coinsData) {
+                    coin in
+                        ZStack {
+                            NavigationLink(destination: CoinDetailsView(coin: coin)) {
+                            }.opacity(0.0)
+                            CoinItemView(coin: coin)
+                        }
+                }
+                .navigationBarTitle("Coins available")
+            }
+        }
+    }
+}
+
+struct ListCoinsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListCoinsView_Previews_Container()
+    }
+}
+
+struct ListCoinsView_Previews_Container: View {
+    @State private var user:User = User()
+    
+    var body: some View {
+        ListCoinsView(childView: .constant(""), user: $user)
+    }
+}
---
+++ Crypstock/Views/Transaction.swift
@@ -0,0 +1,14 @@
+//
+//  Transaction.swift
+//  Crypstock
+//
+//  Created by Maxence Marchal on 08/01/2021.
+//
+
+import Foundation
+
+struct CoinTransaction: Identifiable {
+    let id = UUID()
+    let value: Double;
+    let timestamp: TimeInterval;
+}
